3ccb97d80c602d2679c059c61c3374fa9b312599
Paul Gossman <contact@paulgossman.com>
2017-05-23 15:43:08 +0000

    Initial commit

---
+++ src/functions.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Creates HTML & Bootstrap formatted notification box from session data.
+ *
+ * Looks for "notification" using Laravel's Session facade in the format "{type}|{message content}".
+ * {type} can be either "success", "info", "warning", or "danger"; your choice will affect how the alert will appear
+ * to the user.
+ *
+ * Example:
+ * Laravel redirect: redirect("/")->with("notification", "alert|Your form failed to submit");
+ * Show notification in view: {{!! displayNotification() !!}}
+ *
+ * @return string HTML formatted alert
+ */
+function displayNotification()
+{
+	if (Session::has("notification")) {
+		list($type, $message) = explode('|', Session::get("notification"), 2);
+
+		return sprintf('<div class="alert alert-%s">%s</div>', $type, $message);
+	}
+
+	return "";
+}
+
+/**
+ * Converts to lowercase, trims whitespace, and converts a potential email address to a username by removing anything
+ * that comes after an @ symbol.
+ *
+ * @param string $username
+ * @return string
+ */
+function cleanUsername($username)
+{
+    $usernameArr = explode('@', $username);
+
+    return trim(strtolower($usernameArr[0]));
+}
